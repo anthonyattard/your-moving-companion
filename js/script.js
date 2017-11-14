3bb50c201d98224d27951866d0ab68fd22c5fb2a
Anthony Attard <anthony@anthonyattard.com>
2017-11-13 23:32:40 -0800

    Added streetview AJAX for background images

--- js/script.js
+++ js/script.js
@@ -6,15 +6,22 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var $streetView = $('#streetView');
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
+    $streetView.remove();
 
     // load streetview
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
 
-    // YOUR CODE GOES HERE!
+    $greeting.text('So you want to live at ' + address + '?');
 
+    var $streetURL = "https://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + address;
+    $body.append('<img class="bgimg" id="streetView" src="' + $streetURL + '">' );
     return false;
 };
 
