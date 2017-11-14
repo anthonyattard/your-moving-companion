6155745408acd8919eb33c172fd6ab9372445058
Anthony Attard <anthony@anthonyattard.com>
2017-11-14 00:46:12 -0800

    Added New York Times integration

--- js/script.js
+++ js/script.js
@@ -22,7 +22,42 @@ function loadData() {
 
     var $streetURL = "https://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + address;
     $body.append('<img class="bgimg" id="streetView" src="' + $streetURL + '">' );
+
+
+    // NYTimes AJAX
+    var nyTimesAPIKey = config.NY_TIMES_API_KEY // Replace with your API Key for The New York Times
+    var nyTimesURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+    nyTimesURL += '?' + $.param({
+        'api-key': nyTimesAPIKey,
+        'sort': "newest",
+        'q': cityStr
+    });
+
+    $.getJSON(nyTimesURL, function( data ) {
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+
+        var items = [];
+
+        $.each( articles, function() {
+            items.push('<li class="article">'+
+                '<a href='+this.web_url+'>'+this.headline.main+'</a>'+
+                '<p>'+this.snippet+'</p>'+
+                '</li>');
+        });
+
+        $( "<ul/>", {
+            "id": "nytimes-articles",
+            html: items.join( "" )
+        }).appendTo( $nytElem );
+  
+    });
+
     return false;
+
+
 };
 
 $('#form-container').submit(loadData);
